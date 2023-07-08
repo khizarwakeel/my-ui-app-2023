cdcc40b93199708228c5ba60b497437cf3feffea
Khizar Wakeel <khizarwakeel@gmail.com>
2023-07-08 21:12:49 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
-import Header from './header';
-import Slidered from './slidered';
-import YouMightLike from './youmightlike';
+import "./App.css";
+import Header from "./header";
+import Slidered from "./slidered";
+import YouMightLike from "./youmightlike";
 
 function App() {
-
   return (
     <div>
       <Header />
       <Slidered />
       <YouMightLike />
     </div>
-  )
+  );
 }
 
 export default App;
