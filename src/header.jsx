b308d8821742977f290f0b2348b269c69fea1ade
Khizar Wakeel <khizarwakeel@gmail.com>
2023-07-08 21:18:00 +0500

    addded path

--- src/header.jsx
+++ src/header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <img className="h-8 w-auto" src="../public/assets/Logo.png" alt="" />
+            <img className="h-8 w-auto" src="/assets/Logo.png" alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
